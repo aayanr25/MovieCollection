fbaeca50d8bf6272af8df6d5381028809b77435d
BT_1N3_06 <sample@bths,.edu>
2024-02-15 09:02:02 -0500

    started importData

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,2 +1,55 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MovieCollection {
+    private ArrayList<Movie> movies;
+    private Scanner fileScanner;
+    private Scanner scan;
+
+
+
+
+    public MovieCollection() {
+
+    }
+
+    public void importData() {
+        movies = new ArrayList<>();
+        try {
+            File myFile = new File("src//movies_data.csv");
+            fileScanner = new Scanner(myFile);
+            while (fileScanner.hasNext()) {
+                String data = fileScanner.nextLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void mainMenu() {
+        System.out.println("Welcome to the movie collection!");
+        String menuOption = "";
+
+        while (!menuOption.equals("q")) {
+            System.out.println("------------ Main Menu ----------");
+            System.out.println("- search (t)itles");
+            System.out.println("- search (c)ast");
+            System.out.println("- (q)uit");
+            System.out.print("Enter choice: ");
+            menuOption = scan.nextLine();
+
+            if (menuOption.equals("t")) {
+                searchTitles();
+            } else if (menuOption.equals("c")) {
+                searchCast();
+            } else if (menuOption.equals("q")) {
+                System.out.println("Goodbye!");
+            } else {
+                System.out.println("Invalid choice!");
+            }
+        }
+
+    }
 }
